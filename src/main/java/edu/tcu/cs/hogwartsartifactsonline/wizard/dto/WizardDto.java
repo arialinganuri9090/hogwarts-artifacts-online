6eb78e8f0b8a321a51e5a62efa122fa4ce4d89fb
Aria Linganuri <arialinganuri@gmail.com>
2026-03-24 00:46:16 -0500

    Artifact CRUD done! Close #1

---
+++ src/main/java/edu/tcu/cs/hogwartsartifactsonline/wizard/dto/WizardDto.java
@@ -0,0 +1,11 @@
+package edu.tcu.cs.hogwartsartifactsonline.wizard.dto;
+
+public record WizardDto( Integer Id,
+
+                         String name,
+
+                         Integer numberOfArtifacts) {
+
+
+}
+
